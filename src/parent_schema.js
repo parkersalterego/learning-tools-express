efbe6ffedfe3d9c652fe45444866de8ce93f345a
David Ramirez <dramirez2096@gmail.com>
2017-11-21 18:44:00 -0800

    parent and student schemas added

---
+++ src/parent_schema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const parentSchema = new Schema({
+    personalInfo: {
+        firstname: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+    },
+    loginInfo: {
+        email: {
+            type: String,
+            required: true
+        },
+        password: { 
+            type: String,
+            required: true
+        }
+    },
+    student: {
+        required: true,
+        studentId: [{
+            type: String
+        }]
+    },
+});
+
+module.exports = parentSchema;
